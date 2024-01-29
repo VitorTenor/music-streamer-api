ebec4f93038e366a6f11cb27ea216631d3fd4d80
vitor tenorio <vitortenorio.dev@gmail.com>
2024-01-28 21:19:34 -0300

    Refat

--- app/src/main/java/com/music/musicStreamer/api/v1/client/UserClient.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/client/UserClient.java
@@ -13,50 +13,54 @@ import com.music.musicStreamer.enums.UserMessages;
 import com.music.musicStreamer.exception.UserException;
 import com.music.musicStreamer.gateway.UserGateway;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.logging.Logger;
 
-
+@Slf4j
 @Component
 @AllArgsConstructor
 public class UserClient implements UserGateway {
     private final AuthClient authClient;
     private final UserRegisterFactory userRegisterFactory;
     private final TokenService tokenService;
-    private final UserValidator userValidator;
     private final UserRepository userRepository;
-    private final Logger LOGGER = Logger.getLogger(UserClient.class.getName());
 
     @Override
     @Transactional
     public UserRegisterResponseEntity createUser(UserRegisterRequestEntity user) {
-        LOGGER.info("[UserClient] Create user");
+        log.info("[UserClient] Create user");
 
         final var existsByEmail = userRepository.existsByEmail(user.email());
         if (Boolean.TRUE.equals(existsByEmail)) throw new UserException(UserMessages.ALREADY_EXISTS);
 
         final var createdUser = save(userRegisterFactory.toModel(user));
 
-        LOGGER.info("[UserClient] User created => userId: " + createdUser.getId());
+        log.info("[UserClient] User created => userId: " + createdUser.getId());
 
         return userRegisterFactory.toEntity(createdUser);
     }
 
     @Override
     public UserLoginResponseEntity loginUser(final UserAuthRequest userAuthRequest) {
-        LOGGER.info("[UserClient] Login user");
-        UserModel user = userValidator.validateUserLogin(userAuthRequest);
+        log.info("[UserClient] Login user");
+        final var user = userRepository.findByEmail(userAuthRequest.getEmail())
+                .orElseThrow(() -> {
+                    log.info("[UserClient] User not found => " + userAuthRequest.getEmail());
+                    return new UserException(UserMessages.NOT_FOUND);
+                });
 
         authClient.authenticate(userAuthRequest);
+        final var token = tokenService.generateToken(user);
 
-        LOGGER.info("[UserClient] User logged => " + user.getEmail());
-        return new UserLoginResponseEntity(user.getName(), user.getEmail(), tokenService.generateToken(user));
+        log.info("[UserClient] User logged => " + user.getEmail());
+        return new UserLoginResponseEntity(user.getName(), user.getEmail(), token);
     }
 
     private UserModel save(UserModel userModel) {
-        LOGGER.info("[UserClient] Save user => " + userModel.getEmail());
+        log.info("[UserClient] Save user => " + userModel.getEmail());
         return this.userRepository.save(userModel);
     }
 
--- app/src/main/java/com/music/musicStreamer/core/util/validator/UserValidator.java
+++ app/src/main/java/com/music/musicStreamer/core/util/validator/UserValidator.java
@@ -1,61 +1,25 @@
 package com.music.musicStreamer.core.util.validator;
 
-import com.music.musicStreamer.api.v1.model.UserModel;
 import com.music.musicStreamer.api.v1.repository.UserRepository;
-import com.music.musicStreamer.entity.user.UserAuthRequest;
-import com.music.musicStreamer.entity.user.UserRegisterRequestEntity;
 import com.music.musicStreamer.enums.UserMessages;
 import com.music.musicStreamer.exception.UserException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.logging.Logger;
-import java.util.regex.Pattern;
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserValidator {
     private final UserRepository userRepository;
-    private final Logger LOGGER = Logger.getLogger(UserValidator.class.getName());
-    private final String REGEX_EMAIL = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
-
-    public void validateUser(UserRegisterRequestEntity userRegisterRequestEntity) {
-        LOGGER.info("[UserValidator] Validate user");
-        validateUserEmail(userRegisterRequestEntity.email());
-
-        if (userRegisterRequestEntity.name().isBlank()) throw new UserException(UserMessages.NAME_IS_REQUIRED);
-        if (userRegisterRequestEntity.password().isBlank()) throw new UserException(UserMessages.PASSWORD_IS_REQUIRED);
-        if (userRegisterRequestEntity.password().length() < 6) throw new UserException(UserMessages.PASSWORD_IS_TOO_SHORT);
-        if (userRepository.existsByEmail(userRegisterRequestEntity.email())) throw new UserException(UserMessages.ALREADY_EXISTS);
-
-        LOGGER.info("[UserValidator] User is valid");
-    }
-
-    public UserModel validateUserLogin(UserAuthRequest userAuthRequest) {
-        LOGGER.info("[UserValidator] Validate user login");
-
-        validateUserEmail(userAuthRequest.getEmail());
-        if (userAuthRequest.getPassword().isBlank()) throw new UserException(UserMessages.PASSWORD_IS_REQUIRED);
-        return userRepository.findByEmail(userAuthRequest.getEmail()).orElseThrow(() -> new UserException(UserMessages.NOT_FOUND));
-    }
-
-    public void validateUserEmail(String email) {
-        LOGGER.info("[UserValidator] Validate user email");
-
-        if (email.isBlank()) throw new UserException(UserMessages.EMAIL_IS_REQUIRED);
-        Pattern pattern = Pattern.compile(REGEX_EMAIL);
-        if (!pattern.matcher(email).matches()) throw new UserException(UserMessages.EMAIL_IS_INVALID);
-
-        LOGGER.info("[UserValidator] User email is valid");
-    }
-
 
     public void validateIfExistById(int id) {
-        LOGGER.info("[UserValidator] Validate if user exist by id");
+        log.info("[UserValidator] Validate if user exist by id");
 
-        if (!userRepository.existsById(id)) throw new UserException(UserMessages.NOT_FOUND);
+        if (!userRepository.existsById(id)) {
+            throw new UserException(UserMessages.NOT_FOUND);
+        }
 
-        LOGGER.info("[UserValidator] User exist by id");
+        log.info("[UserValidator] User exist by id");
     }
-
 }
