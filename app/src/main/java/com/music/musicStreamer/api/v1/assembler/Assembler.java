366969d2d07ffda72aa5cd04ca930b80ca2f8ce8
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-04 19:53:39 -0300

    Assembler

--- app/src/main/java/com/music/musicStreamer/api/v1/assembler/Assembler.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/assembler/Assembler.java
@@ -3,6 +3,6 @@ package com.music.musicStreamer.api.v1.assembler;
 /**
  * Interface to facilitate the conversion of {@code entities} to {@code DTOs/Output}.
  */
-public interface Assembler<E, R> {
-    R toOutput(E entity);
+public interface Assembler<E, O> {
+    O toOutput(E entity);
 }
