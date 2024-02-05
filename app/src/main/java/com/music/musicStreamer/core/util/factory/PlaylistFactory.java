5c2e81679eec3a25ed90f0a7017d79f1ff1acada
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-05 00:31:46 -0300

    Feat

--- app/src/main/java/com/music/musicStreamer/core/util/factory/PlaylistFactory.java
+++ app/src/main/java/com/music/musicStreamer/core/util/factory/PlaylistFactory.java
@@ -1,9 +1,7 @@
 package com.music.musicStreamer.core.util.factory;
 
 import com.music.musicStreamer.api.v1.database.model.PlaylistModel;
-import com.music.musicStreamer.api.v1.database.model.PlaylistMusicModel;
 import com.music.musicStreamer.entity.playlist.CreatePlaylistEntity;
-import com.music.musicStreamer.entity.playlist.MusicPlaylistRequest;
 import com.music.musicStreamer.entity.playlist.PlaylistEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,10 +14,6 @@ import java.util.List;
 public class PlaylistFactory {
     public PlaylistEntity toEntity(PlaylistModel playlistModel) {
         return new PlaylistEntity((long) playlistModel.getId(), playlistModel.getName(), List.of());
-    }
-
-    public PlaylistMusicModel createPlaylistMusicModel(MusicPlaylistRequest musicPlaylistRequest) {
-        return new PlaylistMusicModel(musicPlaylistRequest.getPlaylistId(), musicPlaylistRequest.getUserId(), musicPlaylistRequest.getMusicId(), new Date(), new Date());
     }
 
     public PlaylistEntity toEntity(final PlaylistModel playlistModel, final List<Long> musicIds) {
