3c7c98bae79e1cc2cefdc949d156420ee2fc3ea7
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-04 22:20:58 -0300

    Feat

--- domain/src/main/java/com/music/musicStreamer/entity/user/CreateUserEntity.java
+++ domain/src/main/java/com/music/musicStreamer/entity/user/CreateUserEntity.java
@@ -1,4 +1,8 @@
 package com.music.musicStreamer.entity.user;
 
-public record CreateUserEntity(String name, String email, String password) {
+public record CreateUserEntity(
+        String name,
+        String email,
+        String password
+) {
 }
