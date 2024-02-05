338d18a41fff70098bb04de39f48f38775fb7e70
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-05 14:40:20 -0300

    Feature

--- app/src/main/java/com/music/musicStreamer/api/v1/client/PlaylistMusicClient.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/client/PlaylistMusicClient.java
@@ -1,19 +1,19 @@
 package com.music.musicStreamer.api.v1.client;
 
-import com.music.musicStreamer.api.v1.database.model.MusicModel;
 import com.music.musicStreamer.api.v1.database.model.PlaylistMusicModel;
 import com.music.musicStreamer.api.v1.database.repository.MusicRepository;
 import com.music.musicStreamer.api.v1.database.repository.PlaylistMusicRepository;
 import com.music.musicStreamer.core.util.factory.PlaylistMusicFactory;
 import com.music.musicStreamer.entity.music.MusicEntity;
 import com.music.musicStreamer.entity.playlistmusic.PlaylistMusicEntity;
+import com.music.musicStreamer.enums.MusicMessages;
 import com.music.musicStreamer.enums.PlaylistMessages;
+import com.music.musicStreamer.exception.MusicException;
 import com.music.musicStreamer.gateway.PlaylistMusicGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.music.musicStreamer.core.util.factory.LogFactory.info;
@@ -46,7 +46,7 @@ public class PlaylistMusicClient implements PlaylistMusicGateway {
     public Boolean deleteByMusicId(final Long musicId) {
         info(this.getClass(), "Delete music from playlist");
 
-        var playlistMusic = playlistMusicRepository.findByMusicId(musicId.intValue());
+        final var playlistMusic = playlistMusicRepository.findByMusicId(musicId.intValue());
         if (playlistMusic.isEmpty()) {
             info(this.getClass(), "Music not found in playlist, nothing to delete");
             return Boolean.FALSE;
@@ -64,18 +64,16 @@ public class PlaylistMusicClient implements PlaylistMusicGateway {
         info(this.getClass(), "Get music by playlist id");
         info(this.getClass(), "Playlist id: " + id);
 
-        var musicList = new ArrayList<MusicEntity>();
+        final var musicList = playlistMusicRepository.findAllById(id);
 
-        for (var playlistMusicModel : playlistMusicRepository.findAllById(id)) {
-           var music = musicRepository.findAllById(playlistMusicModel.getMusicId());
-            for (MusicModel musicModel : music) {
-                musicList.add(playlistMusicFactory.createMusic(musicModel));
-            }
-        }
-
-        info(this.getClass(), "Music list size: " + musicList.size());
-
-        return musicList;
+        return musicList.stream().parallel().map(
+                playlistMusicModel -> {
+                    final var music = musicRepository.findById(playlistMusicModel.getMusicId()).orElseThrow(
+                            () -> new MusicException(MusicMessages.NOT_FOUND)
+                    );
+                    return playlistMusicFactory.createMusic(music);
+                }
+        ).toList();
     }
 
     @Override
