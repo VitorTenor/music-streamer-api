556efb354e9f8f50eae20b917e778e51925c9849
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-05 11:59:26 -0300

    Feat

--- app/src/main/java/com/music/musicStreamer/api/v1/controller/AbstractController.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/controller/AbstractController.java
@@ -2,8 +2,8 @@ package com.music.musicStreamer.api.v1.controller;
 
 import com.google.gson.Gson;
 import com.music.musicStreamer.core.security.model.UserToken;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,37 +11,36 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
-import java.util.logging.Logger;
+
+import static com.music.musicStreamer.core.util.factory.LogFactory.info;
 
 @Component
-@RequiredArgsConstructor
 public class AbstractController {
 
     private @Autowired HttpServletRequest httpServletRequest;
-    private final Logger LOGGER = Logger.getLogger(AbstractController.class.getName());
-
+    private final String TOKEN_PREFIX = "Bearer";
+    
     public UserToken getUserFromToken() {
-        LOGGER.info("[AbstractController] Get user from token");
+        info(this.getClass(), "Get user from token");
 
         var payload = tokenDecode(getToken());
-        var gson = new Gson();
-        var userToken = gson.fromJson(payload, UserToken.class);
+        var userToken = new Gson().fromJson(payload, UserToken.class);
 
-        LOGGER.info("[AbstractController] User email: " + userToken.getUserEmail());
-        LOGGER.info("[AbstractController] User id: " + userToken.getUserId());
+        info(this.getClass(), "User email: " + userToken.getUserEmail());
+        info(this.getClass(), "User id: " + userToken.getUserId());
 
         return userToken;
     }
 
-    private static String tokenDecode(String token) {
+    private static String tokenDecode(final String token) {
         Base64.Decoder decoder = Base64.getUrlDecoder();
         String[] chunks = token.split("\\.");
         return new String(decoder.decode(chunks[1]));
     }
 
     private String getToken() {
-        String token = httpServletRequest.getHeader("Authorization");
-        return token.replace("Bearer ", "");
+        var token = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        return token.replace(TOKEN_PREFIX, "");
     }
 
     protected <T> ResponseEntity<T> buildResponseEntity(final HttpStatus status, final T response) {
