07bb6e0afe36c75ee51d03f13530d6699391a3dc
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-05 14:53:05 -0300

    Feature

--- app/src/main/java/com/music/musicStreamer/api/v1/controller/UserController.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/controller/UserController.java
@@ -33,8 +33,8 @@ public class UserController extends AbstractController implements UserController
     /*
     * - Assembler
     */
-    private final AuthenticationAssembler authenticationAssembler;
     private final UserAssembler userAssembler;
+    private final AuthenticationAssembler authenticationAssembler;
 
     @Override
     @PostMapping
