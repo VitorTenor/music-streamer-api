fb396bed5767eda5ac3fd4eb5577aa7e0c8a3425
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-05 11:02:02 -0300

    Feat

--- app/src/main/java/com/music/musicStreamer/core/storage/FileBase.java
+++ app/src/main/java/com/music/musicStreamer/core/storage/FileBase.java
@@ -1,12 +1,9 @@
 package com.music.musicStreamer.core.storage;
 
-import org.springframework.stereotype.Component;
-
 import java.io.File;
 import java.io.InputStream;
 import java.util.List;
 
-@Component
 public interface FileBase<T> {
     void saveInFiles(T object, String fileName);
     void deleteInFiles(String fileName);
