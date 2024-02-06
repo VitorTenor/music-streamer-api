d58d65f02c6960910d08c8a8142fae4e3a74fdb7
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-06 15:45:11 -0300

    Feature

--- app/src/main/java/com/music/musicStreamer/api/v1/database/repository/MusicRepository.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/database/repository/MusicRepository.java
@@ -4,10 +4,7 @@ package com.music.musicStreamer.api.v1.database.repository;
 import com.music.musicStreamer.api.v1.database.model.MusicModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MusicRepository extends JpaRepository<MusicModel, Integer> {
 
-    List<MusicModel> findAllById(int musicId);
 }
 
--- app/src/main/java/com/music/musicStreamer/api/v1/database/repository/UserRepository.java
+++ app/src/main/java/com/music/musicStreamer/api/v1/database/repository/UserRepository.java
@@ -2,13 +2,11 @@ package com.music.musicStreamer.api.v1.database.repository;
 
 import com.music.musicStreamer.api.v1.database.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
-    @Query(value = "SELECT * FROM users WHERE email = :email", nativeQuery = true)
     Optional<UserModel> findByEmail(String email);
     Boolean existsByEmail(String email);
     UserDetails findUserDetailsByEmail(String email);
