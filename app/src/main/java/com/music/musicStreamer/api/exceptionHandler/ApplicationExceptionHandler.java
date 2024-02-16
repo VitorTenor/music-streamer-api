7585c66c14bfd3363ba36b43a5e015cb81caf399
vitor tenorio <vitortenorio.dev@gmail.com>
2024-02-16 11:47:40 -0300

    Exception Handler

--- app/src/main/java/com/music/musicStreamer/api/exceptionHandler/ApplicationExceptionHandler.java
+++ app/src/main/java/com/music/musicStreamer/api/exceptionHandler/ApplicationExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.music.musicStreamer.api.exceptionHandler;
 
 import com.google.gson.Gson;
+import com.music.musicStreamer.enums.ProblemType;
 import com.music.musicStreamer.exception.SecurityException;
 import com.music.musicStreamer.exception.*;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +20,10 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 
-@RequiredArgsConstructor
 @ControllerAdvice
+@RequiredArgsConstructor
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
@@ -37,12 +37,12 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
     }
 
     private ResponseEntity<Object> handleValidationInternal(Exception ex, HttpHeaders headers, HttpStatus status, WebRequest request, BindingResult bindingResult) {
-        ProblemType problemType = ProblemType.INVALID_FIELD;
+        final var problemType = ProblemType.INVALID_FIELD;
 
-        List<Problem.Object> list = bindingResult.getAllErrors().stream()
+        var list = bindingResult.getAllErrors().stream()
                 .map(objectError -> {
-                    String message = objectError.getDefaultMessage();
-                    String name = objectError.getObjectName();
+                    final var message = objectError.getDefaultMessage();
+                    var name = objectError.getObjectName();
                     if (objectError instanceof FieldError) {
                         name = ((FieldError) objectError).getField();
                     }
@@ -51,101 +51,103 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
                             .userMessage(message)
                             .build();
                 })
-                .collect(java.util.stream.Collectors.toList());
+                .toList();
 
         var problem = Problem.builder()
                 .timestamp(LocalDateTime.now())
                 .status(status.value())
-                .title(problemType.getTitle())
+                .title(problemType.title())
                 .detail("One or more fields are invalid. Fill in correctly and try again.")
                 .userMessage("One or more fields are invalid. Fill in correctly and try again.")
-                .uri(problemType.getUri())
+                .uri(problemType.uri())
                 .objects(list)
                 .build();
 
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
-        var status = HttpStatus.INTERNAL_SERVER_ERROR;
-        ProblemType problemType = ProblemType.UNEXPECTED_ERROR;
+        final var status = HttpStatus.INTERNAL_SERVER_ERROR;
+        final var problemType = ProblemType.UNEXPECTED_ERROR;
         var problem = Problem.builder()
                 .timestamp(LocalDateTime.now())
                 .status(status.value())
-                .title(problemType.getTitle())
+                .title(problemType.title())
                 .detail(ex.getMessage())
                 .userMessage("An unexpected error occurred. Try again later.")
-                .uri(problemType.getUri())
+                .uri(problemType.uri())
                 .build();
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<Object> handleBusinessException(BusinessException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ProblemType problemType = ProblemType.BUSINESS_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.BAD_REQUEST;
+        final var problemType = ProblemType.BUSINESS_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(PlaylistException.class)
     public ResponseEntity<Object> handlePlaylistException(PlaylistException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ProblemType problemType = ProblemType.PLAYLIST_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.BAD_REQUEST;
+        final var problemType = ProblemType.PLAYLIST_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(UserException.class)
     public ResponseEntity<Object> handleUserException(UserException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ProblemType problemType = ProblemType.USER_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.BAD_REQUEST;
+        final var problemType = ProblemType.USER_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(ImageException.class)
     public ResponseEntity<Object> handleImageException(ImageException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ProblemType problemType = ProblemType.IMAGE_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.BAD_REQUEST;
+        final var problemType = ProblemType.IMAGE_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(SecurityException.class)
     public ResponseEntity<Object> handleSecurityException(SecurityException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.UNAUTHORIZED;
-        ProblemType problemType = ProblemType.SECURITY_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.UNAUTHORIZED;
+        final var problemType = ProblemType.SECURITY_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(MusicException.class)
     public ResponseEntity<Object> handleMusicException(MusicException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        ProblemType problemType = ProblemType.MUSIC_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.BAD_REQUEST;
+        final var problemType = ProblemType.MUSIC_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
     @ExceptionHandler(feign.FeignException.class)
     public ResponseEntity<Object> handleUncaught(feign.FeignException ex, WebRequest request) {
         try {
-            Gson gson = new Gson();
-            Object response = gson.fromJson(ex.contentUTF8(), Object.class);
-            HttpStatus status = HttpStatus.valueOf(ex.status());
+            var gson = new Gson();
+            var response = gson.fromJson(ex.contentUTF8(), Object.class);
+            var status = HttpStatus.valueOf(ex.status());
 
             return handleExceptionInternal(ex, response, new HttpHeaders(), status, request);
         } catch (Exception e ){
-            HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-            ProblemType problemType = ProblemType.UNEXPECTED_ERROR;
+            final var status = HttpStatus.INTERNAL_SERVER_ERROR;
+            final var problemType = ProblemType.UNEXPECTED_ERROR;
 
-            Problem problem = createProblemBuilder(status, problemType, ex.getMessage());
+            var problem = createProblemBuilder(status, problemType, ex.getMessage());
 
             return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
         }
@@ -153,10 +155,10 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.UNAUTHORIZED;
-        ProblemType problemType = ProblemType.SECURITY_ERROR;
-        String detail = ex.getMessage();
-        Problem problem = createProblemBuilder(status, problemType, detail);
+        final var status = HttpStatus.UNAUTHORIZED;
+        final var problemType = ProblemType.SECURITY_ERROR;
+        final var detail = ex.getMessage();
+        var problem = createProblemBuilder(status, problemType, detail);
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
@@ -164,8 +166,8 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         return Problem.builder()
                 .timestamp(LocalDateTime.now())
                 .status(status.value())
-                .uri(problemType.getUri())
-                .title(problemType.getTitle())
+                .uri(problemType.uri())
+                .title(problemType.title())
                 .userMessage(detail)
                 .detail(detail)
                 .build();
@@ -180,5 +182,4 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         logger.error(ex.getMessage(), ex);
         return new ResponseEntity<>(body, headers, status);
     }
-
 }
